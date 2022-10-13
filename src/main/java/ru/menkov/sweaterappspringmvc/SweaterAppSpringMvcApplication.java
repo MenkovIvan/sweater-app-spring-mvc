52bacf5d9a66e7bc4492cd6763720bde2a48278d
Ivan <menkov_99@mail.ru>
2022-10-14 01:14:29 +0300

    Initial commit

---
+++ src/main/java/ru/menkov/sweaterappspringmvc/SweaterAppSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package ru.menkov.sweaterappspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweaterAppSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweaterAppSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/menkov/sweaterappspringmvc/SweaterAppSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.menkov.sweaterappspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterAppSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
